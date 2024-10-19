6183dc115b833e464ca5cbf789ba50e9bf557406
Abdel_BU_JF <redouana@phi-ltsp-03.u-ga.fr>
2024-10-19 16:22:30 +0200

    PARTIE3 V2

--- WEEK6/EXAM/PARTIE3/Votation.java
+++ WEEK6/EXAM/PARTIE3/Votation.java
@@ -44,10 +44,218 @@ class Postulant{
 }
 
 class Scrutin{
-    private int nbPostulants;
     private int nbVotantMax;
+    private int dateScrutin;
+    public ArrayList<Postulant> postulants;
+    private ArrayList<Vote> votes ;
+    public Scrutin(ArrayList<Postulant> post, int nb, int date,boolean bol){
+        postulants = new ArrayList<Postulant>();
+        for(int i =0;i<post.size();i++){
+            postulants.add(new Postulant(post.get(i)));
+            if(bol){
+                postulants.get(i).init();
+            }
+        }
+        this.dateScrutin=date;
+        this.nbVotantMax=nb;
+        this.votes = new ArrayList<Vote>();
+
+    }
+
+    public Scrutin(ArrayList<Postulant> post, int nb, int date){
+        postulants = new ArrayList<Postulant>();
+        for(int i =0;i<post.size();i++){
+            postulants.add(new Postulant(post.get(i)));
+            postulants.get(i).init();
+        }
+        this.dateScrutin=date;
+        this.nbVotantMax=nb;
+        this.votes = new ArrayList<Vote>();
+
+    }
+    
+    public int calculerVotants(){
+        int res=0;
+        for(Postulant e: postulants){
+            res=res+e.getVotes();
+        }
+        return res;
+    }
+
+    public String gagnant(){
+        int tmp=0;
+        for (int i=0;i<postulants.size()-1;i++){
+            if(postulants.get(i).getVotes()<postulants.get(i+1).getVotes()){
+                tmp=i+1;
+            }
+        }
+        return postulants.get(tmp).getNom();
+    }
+
+    public void resultats(){
+        StringBuilder str = new StringBuilder();
+        str.append("Taux de participation -> "+String.format("%.1f",(((double)(calculerVotants())/nbVotantMax))*100)+
+            " pour cent\nNombre effectif de votants -> "+calculerVotants()+
+            "\nLe chef choisi est -> "+gagnant()+"\n\nRépartition des electeurs\n");
+        for(Postulant e:postulants){
+            str.append(e.getNom()+" -> "+String.format("%.1f", ((double)(e.getVotes())/calculerVotants())*100)+" pour cent des électeurs\n");
+        }
+        System.out.print(str.toString()+"\n");    
+    }
+
+    public void compterVotes(){
+        for (Vote e:votes){
+            if(!e.estInvalide()){
+                for(Postulant el:postulants){
+                    if(e.getNom().equals(el.getNom())){
+                        el.elect(); 
+                    }
+                }
+            }
+        }
+    }
+
+    public void simuler(double taux, int date){
+        int nbVotant=(int)(nbVotantMax*taux);
+        int cadNum;
+        for (int i =0;i<nbVotant;i++){
+            cadNum=Utils.randomInt(postulants.size()-1);
+            if(i%3==0){
+                votes.add(new BultinElectronique(postulants.get(cadNum).getNom(), date,dateScrutin));
+            }
+            else if(i%3==1){
+                if (i%2==0){
+                    votes.add(new BulletinPapier(postulants.get(cadNum).getNom(), date,dateScrutin,false));
+                }
+                else{
+                    votes.add(new BulletinPapier(postulants.get(cadNum).getNom(), date,dateScrutin,true));
+
+                }
+            }    
+            else if(i%3==2){
+                if (i%2==0){
+                    votes.add(new BulletinPapier(postulants.get(cadNum).getNom(), date,dateScrutin,false));
+                }
+                else{
+                    votes.add(new BulletinPapier(postulants.get(cadNum).getNom(), date,dateScrutin,true));
+
+                }            
+            }
+
+            System.out.println(votes.get(i).toString());
+        }
+
+    }
+
+}
+
+abstract class Vote{
+    private String nom;
     private int date;
-     
+    private int dateLimite;
+    public Vote(String nom,int date , int datelim){
+        this.nom=nom;
+        this.date=date;
+        this.dateLimite=datelim;
+    }
+
+    public int getDate(){
+        return date;
+    }
+
+    public String getNom(){
+        return nom;
+    }    
+
+    public int getDateLimite(){
+        return dateLimite;
+    }
+    public String toString(){
+        if (date<=dateLimite){
+            return " pour "+nom+" -> valide\n";
+        }
+        else{
+            return " pour "+nom+" -> invalide\n";
+        }
+    }
+    abstract public boolean estInvalide();
+}
+
+
+class BulletinPapier extends Vote{
+    private boolean signe;
+    public BulletinPapier(String nom, int date, int datelim, boolean signe){
+        super(nom, date, datelim);
+        this.signe=signe;
+    }
+    public boolean estInvalide(){
+        return (!signe);
+    }
+    public boolean estSigne(){
+        return signe;
+    }
+    public String toString(){
+        if (estInvalide()){
+            return " vote par bulletin papier pour "+getNom()+" -> invalide";
+        }
+        else{
+            return " vote par bulletin papier pour "+getNom()+" -> valide";
+        }
+    }
+
+}
+
+interface CheckBulltin{
+    boolean checkDate();
+}
+
+class BulletinCourrier  extends BulletinPapier implements CheckBulltin{
+    
+    public BulletinCourrier(String nom, int date, int datelim, boolean signe){
+        super(nom,date,datelim,signe);
+    }
+
+    public boolean checkDate(){
+        return (getDate()>getDateLimite());
+    }
+
+    public boolean estInvalide(){
+        return checkDate()||!estSigne();
+    }  
+    
+    public String toString(){
+        if (estInvalide()){
+            return " envoi par courrier d’un vote par bulletin papier pour "+getNom()+" -> invalide";
+        }
+        else{
+            return " envoi par courrier d’un vote par bulletin papier pour "+getNom()+" -> valide";
+        }
+    }   
+
+}
+
+class BultinElectronique extends Vote{
+
+    public BultinElectronique(String nom, int date, int datelim){
+        super(nom, date, datelim);
+    }
+    public boolean checkDate(){
+        return (getDate()>getDateLimite()-2);
+    }
+    public boolean estInvalide(){
+        return checkDate();
+    }
+
+    public String toString(){
+        if (estInvalide()){
+            return " vote electronique pour "+getNom()+" -> invalide";
+        }
+        else{
+            return " vote electronique pour "+getNom()+" -> valide";
+        }
+    }   
+
+
 }
 /*******************************************
  * Ne pas modifier les parties fournies
@@ -98,7 +306,7 @@ class Votation {
 
         // FIN TEST 1
 
-        /*/ TEST 2
+        // TEST 2
         System.out.println("Test partie II:");
         System.out.println("---------------");
 
@@ -121,7 +329,7 @@ class Votation {
         scrutin.simuler(0.75, 15);
         scrutin.compterVotes();
         scrutin.resultats();
-        //FIN TEST 2*/
+        //FIN TEST 2
 
     }
 }
